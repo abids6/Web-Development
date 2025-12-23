0a3b225e3ad2c42f8436dae917e7ec3c9885bdf4
abids6 <abids6@nycstudents.net>
2025-12-23 03:35:40 +0000

    Update 3.3 practical

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,2 +1,12 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
 
+function describeImage1() {
+document.getElemmentById(¨highlight¨)
+}
+function describeImage2() {
+document.getElementById(¨highlight¨)
+
+}
+function describeImage3() {
+    document.getElemmentById(¨highlight¨)
+}
