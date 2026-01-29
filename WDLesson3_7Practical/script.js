a1d660cbc9ee8559d38d0b216518000e34c19d8b
abids6 <abids6@nycstudents.net>
2026-01-29 14:03:42 +0000

    Update 3.8 practical

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -4,11 +4,31 @@
       3) Display the results in the appropriate element
 */
 
+function calculateDistance() {
+  let x1 = Number(document.getElementById("x1").value);
+  let y1 = Number(document.getElementById("y1").value);
+  let x2 = Number(document.getElementById("x2").value);
+  let y2 = Number(document.getElementById("y2").value);
+
+  let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+  document.getElementById("distanceResult").innerHTML = distance.toFixed(2);
+}
+
+
+
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+function calculateCompoundInterest() {
+  let principal = Number(document.getElementById("principal").value);
+  let rate = Number(document.getElementById("rate").value);
+  let times = Number(document.getElementById("times").value);
+  let years = Number(document.getElementById("years").value);
 
+  let amount = principal * Math.pow(1 + rate / times, times * years);
 
-
+  document.getElementById("interestResult").innerHTML = amount.toFixed(2);
+}
