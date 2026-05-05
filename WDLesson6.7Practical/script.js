67b82c1de1abda1813c659599059160429c2aded
abids6 <abids6@nycstudents.net>
2026-05-05 03:09:08 +0000

    mmm

--- WDLesson6.7Practical/script.js
+++ WDLesson6.7Practical/script.js
@@ -12,19 +12,42 @@ async function init(){
   for(let i = 0; i < data.length; i+=1){
     let collision = data[i];
 
-    build += `<div class="fitted card">
-                <h3>${collision.borough}</h3>
-                <hr>
-                <p>${collision.crash_date}</p>
-                <p>${collision.crash_time}</p>
-                <p>${collision.on_street_name}</p>
-                <hr>
-                <p>Injured: ${collision.number_of_persons_injured}</p>
-                <p>Killed: ${collision.number_of_persons_killed}</p>
-                <hr>
-                <p>${collision.contributing_factor_vehicle_1}</p>
-                <p>${collision.vehicle_type_code1}</p>
-              </div>`;
+    let street = collision.on_street_name;
+
+    if(street == undefined){
+      street = collision.off_street_name;
+    }
+
+    if(street == undefined){
+      street = collision.cross_street_name;
+    }
+
+    let factor = collision.contributing_factor_vehicle_1;
+
+    if(factor == undefined){
+      factor = "No Cause Listed";
+    }
+
+    let vehicle = collision.vehicle_type_code1;
+
+    if(vehicle == undefined){
+      vehicle = "No Vehicle Listed";
+    }
+
+    if(collision.borough != undefined && street != undefined){
+
+      build += `<div class="fitted card">
+                  <h3>${collision.borough}</h3>
+                  <p>${collision.crash_date}</p>
+                  <p>${collision.crash_time}</p>
+                  <p>${street}</p>
+                  <p>Injured: ${collision.number_of_persons_injured}</p>
+                  <p>Killed: ${collision.number_of_persons_killed}</p>
+                  <p>${factor}</p>
+                  <p>${vehicle}</p>
+                </div>`;
+
+    }
   }
 
   output.innerHTML = build;
@@ -40,19 +63,39 @@ function filterByBorough(){
   for(let i = 0; i < data.length; i+=1){
     let collision = data[i];
 
-    if(collision.borough == borough){
+    let street = collision.on_street_name;
+
+    if(street == undefined){
+      street = collision.off_street_name;
+    }
+
+    if(street == undefined){
+      street = collision.cross_street_name;
+    }
+
+    let factor = collision.contributing_factor_vehicle_1;
+
+    if(factor == undefined){
+      factor = "No Cause Listed";
+    }
+
+    let vehicle = collision.vehicle_type_code1;
+
+    if(vehicle == undefined){
+      vehicle = "No Vehicle Listed";
+    }
+
+    if(collision.borough == borough && street != undefined){
+
       build += `<div class="fitted card">
                   <h3>${collision.borough}</h3>
-                  <hr>
                   <p>${collision.crash_date}</p>
                   <p>${collision.crash_time}</p>
-                  <p>${collision.on_street_name}</p>
-                  <hr>
+                  <p>${street}</p>
                   <p>Injured: ${collision.number_of_persons_injured}</p>
                   <p>Killed: ${collision.number_of_persons_killed}</p>
-                  <hr>
-                  <p>${collision.contributing_factor_vehicle_1}</p>
-                  <p>${collision.vehicle_type_code1}</p>
+                  <p>${factor}</p>
+                  <p>${vehicle}</p>
                 </div>`;
 
       ct += 1;
@@ -74,37 +117,55 @@ function filterByBoroughAndInjuries(){
   for(let i = 0; i < data.length; i+=1){
     let collision = data[i];
 
-    if(collision.borough == borough && injuries == "1" && collision.number_of_persons_injured == 1){
+    let street = collision.on_street_name;
+
+    if(street == undefined){
+      street = collision.off_street_name;
+    }
+
+    if(street == undefined){
+      street = collision.cross_street_name;
+    }
+
+    let factor = collision.contributing_factor_vehicle_1;
+
+    if(factor == undefined){
+      factor = "No Cause Listed";
+    }
+
+    let vehicle = collision.vehicle_type_code1;
+
+    if(vehicle == undefined){
+      vehicle = "No Vehicle Listed";
+    }
+
+    if(collision.borough == borough && injuries == "1" && collision.number_of_persons_injured == 1 && street != undefined){
+
       build += `<div class="fitted card">
                   <h3>${collision.borough}</h3>
-                  <hr>
                   <p>${collision.crash_date}</p>
                   <p>${collision.crash_time}</p>
-                  <p>${collision.on_street_name}</p>
-                  <hr>
+                  <p>${street}</p>
                   <p>Injured: ${collision.number_of_persons_injured}</p>
                   <p>Killed: ${collision.number_of_persons_killed}</p>
-                  <hr>
-                  <p>${collision.contributing_factor_vehicle_1}</p>
-                  <p>${collision.vehicle_type_code1}</p>
+                  <p>${factor}</p>
+                  <p>${vehicle}</p>
                 </div>`;
 
       ct += 1;
     }
 
-    if(collision.borough == borough && injuries == "2" && collision.number_of_persons_injured == 2){
+    if(collision.borough == borough && injuries == "2" && collision.number_of_persons_injured == 2 && street != undefined){
+
       build += `<div class="fitted card">
                   <h3>${collision.borough}</h3>
-                  <hr>
                   <p>${collision.crash_date}</p>
                   <p>${collision.crash_time}</p>
-                  <p>${collision.on_street_name}</p>
-                  <hr>
+                  <p>${street}</p>
                   <p>Injured: ${collision.number_of_persons_injured}</p>
                   <p>Killed: ${collision.number_of_persons_killed}</p>
-                  <hr>
-                  <p>${collision.contributing_factor_vehicle_1}</p>
-                  <p>${collision.vehicle_type_code1}</p>
+                  <p>${factor}</p>
+                  <p>${vehicle}</p>
                 </div>`;
 
       ct += 1;
